082212f75bb8cc5eec56dd03f3b32a7fe451d067
Alex Shinko <alexandr_shinko96@mail.ru>
2022-11-21 00:07:36 +0300

    example

--- backend/api/paginators.py
+++ backend/api/paginators.py
@@ -1,5 +1,9 @@
 from rest_framework.pagination import PageNumberPagination
 
+PAGE_LIMIT = 3
+
 
 class PageLimitPagination(PageNumberPagination):
-    page_size_query_param = 'limit'
+    page_size_query_param = PAGE_LIMIT
+
+# 1
