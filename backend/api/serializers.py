e18911b714b44793b078ee222b3efc2baa459d64
Alex Shinko <alexandr_shinko96@mail.ru>
2022-11-20 23:46:35 +0300

    last chance

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -256,6 +256,12 @@ class SubscriptionSerializer(serializers.ModelSerializer):
                   'is_subscribed', 'recipes', 'recipes_count')
         model = User
 
+    def get_is_subscribed(self, obj):
+        return (Subscription.objects.filter(
+            user=self.context['request'].user,
+            following=obj).exists()
+        )
+
     def get_recipes(self, obj):
         request = self.context.get('request')
         recipes = obj.recipes.all()[:request.GET.get('recipes_limit')]
