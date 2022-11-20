50c8335c8e998d1ec3e30642cacdd661da7876bd
Alex Shinko <alexandr_shinko96@mail.ru>
2022-11-20 21:37:04 +0300

    last chance 2.1

--- backend/api/paginators.py
+++ backend/api/paginators.py
@@ -1,5 +1,7 @@
 from rest_framework.pagination import PageNumberPagination
 
+PAGE_SIZE = 3
+
 
 class PageLimitPagination(PageNumberPagination):
-    page_size_query_param = 'limit'
+    page_size_query_param = PAGE_SIZE
